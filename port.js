9ffd31578a46928f0f9e45191c74a0da63e30796
sofiamaia <12300969@aluno.cotemig.com.br>
2024-11-02 21:26:54 -0300

    Add files via upload

---
+++ port.js
@@ -0,0 +1,9 @@
+const toggleButton = document.getElementById('toggle-theme');
+
+
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+    
+    body.classList.toggle('dark-mode');
+});
